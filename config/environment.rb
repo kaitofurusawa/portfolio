16ee26813f27f637d040dd0652f1be41b40d4341
Apple <apple@appurunoMacBook-Air.local>
2025-03-14 07:33:44 +0900

    Deploy to Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+config.assets.compile = true
