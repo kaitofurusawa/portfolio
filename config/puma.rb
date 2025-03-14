63dbe145b73a508985c28c457f4fe4440db0439a
Apple <apple@appurunoMacBook-Air.local>
2025-03-15 03:20:13 +0900

    Disable cluster mode in Puma for Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,13 @@
 # Puma configuration file.
 
-workers ENV.fetch("WEB_CONCURRENCY", 2)
+#workers ENV.fetch("WEB_CONCURRENCY", 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 threads threads_count, threads_count
 
 preload_app!
+
+# Heroku の PORT を使用
+port ENV.fetch("PORT") { 3000 }
 
 environment ENV.fetch("RAILS_ENV") { "development" }
 
