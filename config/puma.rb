fe90559d289ef238ae16157564154fbb1298ba0a
Apple <apple@appurunoMacBook-Air.local>
2025-03-14 16:37:18 +0900

    Fix Puma config for Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,14 @@
-workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
-threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+# Puma configuration file.
+
+port ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
 preload_app!
 
-rackup DefaultRackup
-port ENV.fetch("PORT", 3000)
-environment ENV.fetch("RAILS_ENV") { "production" }
-
 on_worker_boot do
-  ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
