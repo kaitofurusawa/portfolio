fdd0798aa1b12bd663ea25936494ee0da8960f80
Apple <apple@appurunoMacBook-Air.local>
2025-06-26 14:11:10 +0900

    fix: add missing controllers/auto_resize_controller.js to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,4 +5,9 @@
 //= link user_icon.png
 //= link comments.js
 //= link autocomplete.js
+
 //= link controllers/application.js
+//= link controllers/auto_resize_controller.js
+//= link controllers/comment_controller.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
